f8c3e4ed376ddbf1966fecae3ab974a3904cf845
DawidZiec <ziec.dawid@gmail.com>
2019-04-05 18:19:35 +0200

    Added home page

---
+++ WebDev/learning_logs/urls.py
@@ -0,0 +1,12 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Home page
+    url(r'^$', views.index, name='index')
+]
